2130c253be9e0ea5b979838e480cc774098fa701
GhostFairy <38088468+GhostFairy@users.noreply.github.com>
2022-09-15 16:04:06 +0900

    입력 방식 변경

--- BAEKJOON/Baekjoon2920.java
+++ BAEKJOON/Baekjoon2920.java
@@ -1,43 +1,47 @@
 // 2920. 음계
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Baekjoon2920 {
 
-    public static void main(String[] args) {
-        int[][] arr = new int[3][8];
-        arr[0] = new int[]{1, 2, 3, 4, 5, 6, 7, 8};
-        arr[1] = new int[]{8, 7, 6, 5, 4, 3, 2, 1};
-        arr[2] = new int[]{8, 1, 7, 2, 6, 3, 5, 4};
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] temp = br.readLine().split(" ");
+        int[] arr = new int[temp.length];
         for (int i = 0; i < arr.length; i++) {
-            int flag = 0; // 0 = mixed, 1 = ascending, -1 = descending
+            arr[i] = Integer.parseInt(temp[i]);
+        }
 
-            if (arr[i][0] == 1) {
-                flag = 1;
+        int flag = 0; // 0 = mixed, 1 = ascending, -1 = descending
 
-                for (int j = 0; j < arr[i].length - 1; j++) {
-                    if (arr[i][j] + 1 != arr[i][j + 1]) {
-                        flag = 0;
-                        break;
-                    }
-                }
-            } else if (arr[i][0] == 8) {
-                flag = -1;
+        if (arr[0] == 1) {
+            flag = 1;
 
-                for (int j = 0; j < arr[i].length - 1; j++) {
-                    if (arr[i][j] - 1 != arr[i][j + 1]) {
-                        flag = 0;
-                        break;
-                    }
+            for (int i = 0; i < arr.length - 1; i++) {
+                if (arr[i] + 1 != arr[i + 1]) {
+                    flag = 0;
+                    break;
                 }
             }
+        } else if (arr[0] == 8) {
+            flag = -1;
 
-            if (flag == 1) {
-                System.out.println("ascending");
-            } else if (flag == -1) {
-                System.out.println("descending");
-            } else {
-                System.out.println("mixed");
+            for (int i = 0; i < arr.length - 1; i++) {
+                if (arr[i] - 1 != arr[i + 1]) {
+                    flag = 0;
+                    break;
+                }
             }
+        }
+
+        if (flag == 1) {
+            System.out.println("ascending");
+        } else if (flag == -1) {
+            System.out.println("descending");
+        } else {
+            System.out.println("mixed");
         }
     }
 
