acbabca654cb9718f93a98cb2ba55481fbcf5f97
GhostFairy <38088468+GhostFairy@users.noreply.github.com>
2022-09-15 15:30:52 +0900

    입력 방식 변경

--- BAEKJOON/Baekjoon2798.java
+++ BAEKJOON/Baekjoon2798.java
@@ -1,28 +1,29 @@
 // 2798. 블랙잭
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
 
 public class Baekjoon2798 {
 
     public static void main(String[] args) throws IOException {
-        // 데이터 입력
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        int target = sc.nextInt();
-        int[] card = new int[num];
-        for (int i = 0; i < num; i++) {
-            card[i] = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] lineF = br.readLine().split(" ");
+        int N = Integer.parseInt(lineF[0]);
+        int M = Integer.parseInt(lineF[1]);
+
+        String[] temp = br.readLine().split(" ");
+        int[] card = new int[N];
+        for (int i = 0; i < N; i++) {
+            card[i] = Integer.parseInt(temp[i]);
         }
-        sc.close();
 
         int approach = 0;
-        for (int i = 0; i < num - 2; i++) {
-            for (int j = i + 1; j < num - 1; j++) {
-                for (int k = j + 1; k < num; k++) {
+        for (int i = 0; i < N - 2; i++) {
+            for (int j = i + 1; j < N - 1; j++) {
+                for (int k = j + 1; k < N; k++) {
                     int sum = card[i] + card[j] + card[k];
-                    // 목표값에 가장 근접한 값 = 목표값보다 작거나 같은 값 중 가장 큰 값
-                    if (sum <= target && sum > approach) {
+                    if (sum <= M && sum > approach) {
                         approach = sum;
                     }
                 }
